3ce5e06a0d77a9ebcf571ceedc052de2714ecf0a
enrique199413@gmail.com <enrique199413@gmail.com>
2017-05-13 16:29:30 -0500

    #3 add the principal to use in main i18n

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import environment from './environment';
+import Backend from 'i18next-xhr-backend';
+
+Promise.config({
+  warnings: {
+    wForgottenReturn: false
+  }
+});
 
 export function configure(aurelia) {
   aurelia.use
@@ -12,6 +19,22 @@ export function configure(aurelia) {
   if (environment.testing) {
     aurelia.use.plugin('aurelia-testing');
   }
+
+  //register the plugin
+  aurelia.use.plugin('aurelia-i18n', (instance) => {
+    // register backend plugin
+    instance.i18next.use(Backend);
+
+    return instance.setup({
+      backend: {
+        loadPath: './locales/{{lng}}/{{ns}}.json',
+      },
+      lng : 'es',
+      attributes : ['t','i18n'],
+      fallbackLng : 'en',
+      debug : false
+    });
+  });
 
   aurelia.start().then(() => aurelia.setRoot());
 }
